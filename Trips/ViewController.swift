4906496cd1e053c2eebb6a5fd224f7332b33f519
Bruno Alves <alves.bruno945@gmail.com>
2020-12-07 12:24:42 -0300

    incluindo lista de cidades

--- Trips/ViewController.swift
+++ Trips/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var tableTrips: UITableView!
+    
+    //MARK: - Variaveis
+    var listCity: Array<String> = ["Rio de Janeiro", "Ceara", "São Paulo"]
+    
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableTrips.dataSource = self
     }
-
-
+    
+    //MARK: - Table View Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listCity.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = listCity[indexPath.row]
+        
+        return cell
+    }
 }
 
